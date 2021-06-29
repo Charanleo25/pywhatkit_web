4e204d4d0d8b48d7dcd8be9bae3c76cf3f1df0f4
Charan Leo25 <33962520+Charanleo25@users.noreply.github.com>
2021-06-29 15:55:22 +0530

    updated js file

--- PywhatKit/script.js
+++ PywhatKit/script.js
@@ -34,37 +34,6 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     })
-    var typed = new Typed(".typing", {
-        strings: ["Discord","maharashtra"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+     
 
-    var typed = new Typed(".typing-2", {
-        strings: ["Front end developer", "Web developer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
 });
